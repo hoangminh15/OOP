8d7e95c45d73f57f05e4f5ef9e0416b30bc78533
DuongKien2001 <74645733+DuongKien2001@users.noreply.github.com>
2020-11-23 11:38:07 +0700

    Add files via upload

--- src/main/java/Sentence/iSentence.java
+++ src/main/java/Sentence/iSentence.java
@@ -1,5 +1,12 @@
 package Sentence;
 
 public interface iSentence {
-    public String generateSentence();
+	public String generateSentence();
+	public String getReferencePrice();
+	public String giaCP();
+	public String GiaTranSan(String dateData, String masan, String macophieu);
+	public String giaCoPhieu(String dateData, String masan, String macophieu);
+	public String khoiLuongGD();
+	public String soSanhGD(String dateData, String masan, String macophieu);
+	
 }
